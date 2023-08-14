2965e82b2edfe5e6d4635ece719d84fd34c5142e
karis-m <moses.kamau@student.moringaschool.com>
2023-08-14 18:28:45 -0400

    Done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
+import io
+import sys
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!\n")
+    
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number/2
